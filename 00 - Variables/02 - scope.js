03214829cdb61b990ff47a9afa308b76ea623be9
Kapelucito <95049947+Kapelu@users.noreply.github.com>
2022-03-02 00:42:12 -0300

    Alcance de scope

--- 00 - Variables/02 - scope.js
+++ 00 - Variables/02 - scope.js
@@ -16,6 +16,5 @@ saludo1()
 
 /* Para entenderlo mejor observaremos que si llamamos a traves de un console.log a las variables nombre y nombre1, nos daremos cuenta que nombre1 al estar declarada dentro de una funcion con 'var' no es posible llamarla desde afuera dela función.
 */
-
 console.log(`${nombre}`)
 console.log(`${nombre1}`)
