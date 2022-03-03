21218dcd4fe7433fff287024d2ff02e1df400a0a
Kapelucito <95049947+Kapelu@users.noreply.github.com>
2022-03-02 22:37:14 -0300

    Operadores aritméticos

--- 01 - Operadores/00 - operadoresAritmeticos.js
+++ 01 - Operadores/00 - operadoresAritmeticos.js
@@ -11,7 +11,8 @@ console.log( "Resultado de la mult:" + z);
 z = a / b;
 console.log( "Resultado de la division:" + z);
 
-z = a % b;//residuo de la division
+//residuo de la division
+z = a % b;
 console.log( "Resultado de operacion modulo (residuo):" + z);
 
 z = a ** b;
